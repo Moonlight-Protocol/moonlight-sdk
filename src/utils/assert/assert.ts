50bb5426965bf466225fd99984acd2764c2df6da
Fabricius Zatti <fazzatti@gmail.com>
2025-11-05 11:43:56 -0300

    refactor(assert): update error type to Error for improved flexibility and clarity

--- src/utils/assert/assert.ts
+++ src/utils/assert/assert.ts
@@ -1,10 +1,13 @@
-import type { MoonlightError } from "../../error/index.ts";
-
-// Asserts that the given condition is true.
-// Throws the provided error if the condition is false.
-export function assert(
-  condition: unknown,
-  error: MoonlightError
-): asserts condition {
-  if (!condition) throw error;
+/**
+ * Asserts that the given condition is true.
+ * Throws the provided error if the condition is false.
+ *
+ * @param condition - The condition to check
+ * @param error - The error to throw if condition is false
+ * @throws  The provided error if condition is false
+ */
+export function assert(condition: unknown, error: Error): asserts condition {
+  if (!condition) {
+    throw error;
+  }
 }
